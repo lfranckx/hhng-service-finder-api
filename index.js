706a784f17194abbc4dbf8d0a57c9841332dad65
Bowie <lfranckx@thinkful.com>
2023-04-28 09:40:45 -0500

    fix accidental paste

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(cors({ origin: 'https://lfranckx.github.io' }));
+
 app.get('/stylistsAPI', async (req, res) => {
     const apiUrl = 'https://www.hairhasnogender.com/_functions/stylistsAPI';
     try {
@@ -20,3 +27,6 @@ app.get('/stylistsAPI', async (req, res) => {
         res.status(error.response.status).send(error.response.data);
     }
 });  
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Proxy server listening on port ${PORT}`));
