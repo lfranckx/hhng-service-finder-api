eb56da81aae17d5ee0d7afd8bfe0e9577628d76c
Bowie <lfranckx@thinkful.com>
2023-04-28 09:38:53 -0500

    update request headers

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-
-const app = express();
-app.use(cors({ origin: 'https://lfranckx.github.io' }));
-
 app.get('/stylistsAPI', async (req, res) => {
     const apiUrl = 'https://www.hairhasnogender.com/_functions/stylistsAPI';
     try {
         const response = await axios.get(apiUrl, {
-        headers: req.headers,
+            headers: {
+                ...req.headers,
+                host: new URL(apiUrl).host,
+            },
         });
         console.log('response headers...', response.headers);
         console.log('response data...', response.data);
-        res.set(response.headers);
+
+        for (const [key, value] of Object.entries(response.headers)) {
+            res.setHeader(key, value);
+        }
+
         res.send(response.data);
     } catch (error) {
         console.log('catch error...', error);
         res.status(error.response.status).send(error.response.data);
     }
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Proxy server listening on port ${PORT}`));
+});  
